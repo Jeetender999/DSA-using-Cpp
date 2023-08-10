0d685aae49da656c4e638e18b062a8fee8cc6884
Jeetender <999Jeetender@gmail.com>
2023-08-10 15:22:38 +0530

    Bit magic commit

---
+++ Bit Magic/SetBit.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+bool kthBit(int num, int k){
+
+    if(num & 1<<(k-1)){
+        cout << "Yes " << k <<"th bit is a Set bit" << endl;
+    }
+    else{
+        cout << "No, not a set bit" << endl;
+    }
+
+}
+
+int main(){
+   
+  kthBit(5,4);
+}
