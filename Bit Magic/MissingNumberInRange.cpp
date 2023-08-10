20ba4fd4ae28d2c68aead5c60c8af828abe57fad
Jeetender <999Jeetender@gmail.com>
2023-08-11 02:03:54 +0530

    Find the missing number from the given range

---
+++ Bit Magic/MissingNumberInRange.cpp
@@ -0,0 +1,25 @@
+#include<iostream>>
+
+using namespace std;
+
+
+int main(){
+    int arr[] = {1,3,4,5};
+
+    int start = 1;
+    int end = 5;
+    int res1=0;
+    int res2=0;
+
+    for(int i=0;i<4;i++){
+       res1 = res1^arr[i];
+    }
+
+   
+    for(int i=start; i<=end;i++){
+        res2 = res2 ^ i;
+    }
+
+
+    cout << "The missing number is " << (res1^res2 )<<endl;
+}
