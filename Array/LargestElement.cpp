d68d9764c99f5b556cbe86e3153923d8625b223f
Jeetender <999Jeetender@gmail.com>
2023-08-12 02:53:45 +0530

    Index of largest element

--- Array/LargestElement.cpp
+++ Array/LargestElement.cpp
@@ -7,8 +7,8 @@ int largestElement(int arr[], int n){
     int lar = 0;
   
     for(int i=0;i<n;i++){
-        if(arr[i] > lar){
-            lar = arr[i];
+        if(arr[i] > arr[lar]){
+            lar = i;
         }
     }
     return lar;
@@ -16,7 +16,7 @@ int largestElement(int arr[], int n){
 int main(){
     int arr[] = {34,2,3,55,2,35,22,3};
     int n = sizeof(arr)/sizeof(arr[0]);
-   cout << "Hello Worlld" << endl;
+
     int lar = largestElement(arr,n);
-    cout << lar << endl;
+    cout << arr[lar] << endl;
 }
