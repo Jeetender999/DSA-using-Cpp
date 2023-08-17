5ef1a46c126ae3e903af75d570469c6617a94f47
Jeetender <999Jeetender@gmail.com>
2023-08-17 19:51:52 +0530

    Missing number

--- Bit Magic/MissingNumberInRange.cpp
+++ Bit Magic/MissingNumberInRange.cpp
@@ -4,23 +4,24 @@ using namespace std;
 
 //Program to find Missing number from given range
 
+int missingNum(int arr[],int n){
+      int res = 0;
+        
+        for(int i=0;i< n ;i++){
+            res = res ^ arr[i];
+        }
+
+        for(int i=0;i<=n+1;i++){
+            res = res^i;
+        }
+ 
+        return res;
+}
+
 int main(){
     int arr[] = {1,3,4,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << n << endl;
 
-    int start = 1;
-    int end = 5;
-    int res1=0;
-    int res2=0;
-
-    for(int i=0;i<4;i++){
-       res1 = res1^arr[i];
-    }
-
-   
-    for(int i=start; i<=end;i++){
-        res2 = res2 ^ i;
-    }
-
-
-    cout << "The missing number is " << (res1^res2 )<<endl;
+    cout << missingNum(arr,n) << endl;
 }
